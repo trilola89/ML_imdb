459d78442fcce2e1b68c440245f125e6f6ba63bd
Temiko <temikochichinadze@gmail.com>
2023-08-18 23:01:38 -0400

    created helper_functions.py, currently containing a function to preprocess text
    one_hot encoded labels
    
    wrote data to preprocessed.pkl

---
+++ helper_functions.py
@@ -0,0 +1,48 @@
+#preprocessing text
+import nltk
+nltk.download('stopwords')
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import re
+
+
+
+def preprocess_text(text, max_length, tokenizer=None):
+    """
+    Preprocesses the text for an LSTM model.
+
+    Parameters:
+        text (str): The input text to be preprocessed.
+        max_length (int): Maximum length for the padded sequences.
+        tokenizer (Tokenizer, optional): If provided, uses this tokenizer; otherwise, fits a new one.
+
+    Returns:
+        numpy.array: The preprocessed text sequences.
+        Tokenizer: The tokenizer used/fitted on the text.
+    """
+    # Removing HTML tags
+    text = BeautifulSoup(text, "html.parser").get_text()
+
+    # Removing punctuation and converting to lowercase
+    text = re.sub('[^a-zA-Z\s]', '', text).lower()
+
+    # Removing stopwords
+    stops = set(stopwords.words("english"))
+    text = " ".join([word for word in text.split() if word not in stops])
+
+    # Tokenizing and converting to sequences
+    if tokenizer is None:
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts([text])
+
+    sequences = tokenizer.texts_to_sequences([text])
+
+    # Padding the sequences
+    padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post')
+
+    return padded_sequences, tokenizer
+
+
+    
